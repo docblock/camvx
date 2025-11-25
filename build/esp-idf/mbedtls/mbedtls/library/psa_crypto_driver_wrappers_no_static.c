66b440ffb52e6446d0bb9fac858663e89b2969af
Andreas Block <andreas.block@bs-an.de>
2025-11-25 15:45:03 +0100

    Add .gitignore and remove build artifacts from tracking

--- build/esp-idf/mbedtls/mbedtls/library/psa_crypto_driver_wrappers_no_static.c
+++
@@ -1,1 +0,0 @@
-/home/andreas/esp-idfs/v5.5.1/esp-idf/components/mbedtls/mbedtls/library/psa_crypto_driver_wrappers_no_static.c
--- build/esp-idf/mbedtls/mbedtls/library/ssl_debug_helpers_generated.c
+++
@@ -1,1 +0,0 @@
-/home/andreas/esp-idfs/v5.5.1/esp-idf/components/mbedtls/mbedtls/library/ssl_debug_helpers_generated.c
